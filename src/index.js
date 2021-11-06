bc48dbb5370eff06c8ad9154b112f87eee0eae1a
Jimmy Arango <48387416+jimalaros@users.noreply.github.com>
2021-11-06 17:14:42 -0500

    cambios

--- src/index.js
+++ src/index.js
@@ -1,21 +1,30 @@
 import express from 'express';
+import cors from 'cors';
 import helmet from 'helmet';
 import swaggerUI from 'swagger-ui-express';
 import swaggerJSDoc from 'swagger-jsdoc';
-import './basededatos';
-import usuariosRoutes from './routes/usuarios.routes';
-import productosRoutes from './routes/productos.routes';
-import ordenesRoutes from './routes/pedidos.routes';
-import MediosdePagoRoutes from './routes/MediosdePago.routes';
-import * as options from './utils/swagger';
+import './basededatos.js';
+import data from './config.js'
+import usuariosRoutes from './routes/usuarios.routes.js';
+import productosRoutes from './routes/productos.routes.js';
+import ordenesRoutes from './routes/pedidos.routes.js';
+import MediosdePagoRoutes from './routes/MediosdePago.routes.js';
+import * as options from './utils/swagger.js';
 
 const swaggerSpecs = swaggerJSDoc(options.swaggerOptions);
 const app = express();
 
+const cspDefaults = helmet.contentSecurityPolicy.getDefaultDirectives();
+delete cspDefaults['upgrade-insecure-requests'];
+app.use(helmet({
+        contentSecurityPolicy: {directives: cspDefaults}
+}));
+
 app.use(express.json());
-app.use(helmet());
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpecs));
-app.set('puerto', process.env.PORT || 5000);
+app.use(cors());
+
+app.use('/api', swaggerUI.serve, swaggerUI.setup(swaggerSpecs));
+app.set('puerto', data.port);
 
 app.use('/usuarios', usuariosRoutes);
 app.use('/productos', productosRoutes);
